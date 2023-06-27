ea8d6315c8a916bbdae8272607b43f50e0451080
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2023-06-27 13:05:14 +0530

    oop concepts

---
+++ oopclass/objectclass.java
@@ -0,0 +1,11 @@
+package oopclass;
+
+public class objectclass {
+    public static void main(String args[]){
+        phone iphone=new phone();
+        iphone.memory=234;
+        iphone.name="13promax";
+        iphone.version=14;
+        iphone.phoneinfo(iphone);
+    }
+} 
